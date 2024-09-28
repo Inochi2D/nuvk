aa7af066c6eb08adee02ead3d014cb5be2ccf453
Luna <cli-luna@protonmail.com>
2024-09-28 16:33:09 +0200

    Move shader compiler to dlsl dir

---
+++ source/nuvk/dlsl/package.d
@@ -0,0 +1,14 @@
+/*
+    Copyright © 2024, Inochi2D Project
+    Distributed under the 2-Clause BSD License, see LICENSE file.
+    
+    Authors: Luna Nielsen
+*/
+
+/**
+    DLSL (DLang Shader Language)
+
+    Implementation of a shader language with D-inspired syntax.
+*/
+module nuvk.dlsl;
+
