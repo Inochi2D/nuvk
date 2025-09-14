cf218e094e433dbc4a031fffa875999b8c9be258
LunaTheFoxgirl <luna@foxgirls.gay>
2025-09-14 12:04:53 +0200

    VK_KHR_push_descriptor

---
+++ source/vulkan/khr_push_descriptor.d
@@ -0,0 +1,23 @@
+/**
+    VK_KHR_push_descriptor
+    
+    Copyright:
+        Copyright © 2015-2025, The Khronos Group Inc.
+
+    License:    $(LINK2 https://www.apache.org/licenses/LICENSE-2.0, Apache-2.0)
+*/
+module vulkan.khr_push_descriptor;
+import vulkan.core;
+
+extern (System) @nogc nothrow:
+
+// TODO: Implement core part
+version(none):
+
+enum uint VK_KHR_PUSH_DESCRIPTOR_SPEC_VERSION = 2;
+enum string VK_KHR_PUSH_DESCRIPTOR_EXTENSION_NAME = "VK_KHR_push_descriptor";
+
+alias VkPhysicalDevicePushDescriptorPropertiesKHR = VkPhysicalDevicePushDescriptorProperties;
+
+alias PFN_vkCmdPushDescriptorSetKHR = void function(VkCommandBuffer commandBuffer, VkPipelineBindPoint pipelineBindPoint, VkPipelineLayout layout, uint32_t set, uint32_t descriptorWriteCount, const VkWriteDescriptorSet* pDescriptorWrites);
+alias PFN_vkCmdPushDescriptorSetWithTemplateKHR = void function(VkCommandBuffer commandBuffer, VkDescriptorUpdateTemplate descriptorUpdateTemplate, VkPipelineLayout layout, uint32_t set, const void* pData);
