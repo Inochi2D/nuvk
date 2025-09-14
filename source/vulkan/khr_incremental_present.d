c3728bbeee9bb2c83c2058d4072bdd37d6669c59
LunaTheFoxgirl <luna@foxgirls.gay>
2025-09-14 12:04:58 +0200

    VK_KHR_incremental_present

---
+++ source/vulkan/khr_incremental_present.d
@@ -0,0 +1,33 @@
+/**
+    VK_KHR_incremental_present
+    
+    Copyright:
+        Copyright © 2015-2025, The Khronos Group Inc.
+
+    License:    $(LINK2 https://www.apache.org/licenses/LICENSE-2.0, Apache-2.0)
+*/
+module vulkan.khr_incremental_present;
+import vulkan.core;
+
+extern (System) @nogc nothrow:
+
+enum VK_KHR_INCREMENTAL_PRESENT_SPEC_VERSION = 2;
+enum VK_KHR_INCREMENTAL_PRESENT_EXTENSION_NAME = "VK_KHR_incremental_present";
+
+struct VkRectLayerKHR {
+    VkOffset2D offset;
+    VkExtent2D extent;
+    uint layer;
+}
+
+struct VkPresentRegionKHR {
+    uint rectangleCount;
+    const(VkRectLayerKHR)* pRectangles;
+}
+
+struct VkPresentRegionsKHR {
+    VkStructureType sType;
+    const(void)* pNext;
+    uint swapchainCount;
+    const(VkPresentRegionKHR)* pRegions;
+}
