451ffdcf1eeb767962e0036ce99dc6038c535674
LunaTheFoxgirl <luna@foxgirls.gay>
2025-09-14 12:38:12 +0200

    VK_KHR_external_fence & VK_KHR_external_fence_capabilities

---
+++ source/vulkan/khr_external_fence.d
@@ -0,0 +1,19 @@
+/**
+    VK_KHR_external_fence
+    
+    Copyright:
+        Copyright © 2015-2025, The Khronos Group Inc.
+
+    License:    $(LINK2 https://www.apache.org/licenses/LICENSE-2.0, Apache-2.0)
+*/
+module vulkan.khr_external_fence;
+import vulkan.khr_external_memory;
+import vulkan.core;
+
+extern (System) @nogc nothrow:
+
+enum VK_KHR_EXTERNAL_FENCE_SPEC_VERSION = 1;
+enum VK_KHR_EXTERNAL_FENCE_EXTENSION_NAME = "VK_KHR_external_fence";
+
+alias VkFenceImportFlagsKHR = VkFenceImportFlags;
+alias VkExportFenceCreateInfoKHR = VkExportFenceCreateInfo;
